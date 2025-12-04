cb3d1d0c0b681e50c8e24b64ca516daa9927794e
Galobiano <151698267+Galobiano@users.noreply.github.com>
2025-12-04 16:44:43 +0800

    fix: router of login

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 
 function App() {
   return (
-    <>
-      <Login />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
