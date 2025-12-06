bbe3ba9eb1292410ee07ef3fc8b301f68a696cff
Galobiano <151698267+Galobiano@users.noreply.github.com>
2025-12-06 15:33:40 +0800

    feat: added data

---
+++ src/contants/user.ts
@@ -0,0 +1,50 @@
+interface UserDetails {
+  no: number;
+  name: string;
+  username: string;
+  userrole: string;
+  status: string;
+  lastlogin?: string;
+}
+export const user: UserDetails[] = [
+  {
+    no: 1,
+    name: "Alvin",
+    username: "Admin",
+    userrole: "Admin",
+    status: "Active",
+    lastlogin: "November 30, 2023, 2:03:29 pm",
+  },
+  {
+    no: 2,
+    name: "Gerelyn",
+    username: "user",
+    userrole: "user",
+    status: "Active",
+    lastlogin: "",
+  },
+  {
+    no: 3,
+    name: "Hapin",
+    username: "User",
+    userrole: "User",
+    status: "Active",
+    lastlogin: "",
+  },
+  {
+    no: 4,
+    name: "Nacef",
+    username: "Admin",
+    userrole: "Admin",
+    status: "Active",
+    lastlogin: "",
+  },
+  {
+    no: 5,
+    name: "Alvin",
+    username: "Admin",
+    userrole: "Admin",
+    status: "Active",
+    lastlogin: "November 30, 2023, 2:03:29 pm",
+  },
+];
