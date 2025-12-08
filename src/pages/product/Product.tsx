2826d4fc2a78c4a81dc4a2dcc6fae999173501ce
Galobiano <151698267+Galobiano@users.noreply.github.com>
2025-12-08 13:53:20 +0800

    feat: import component + added component

--- src/pages/product/Product.tsx
+++ src/pages/product/Product.tsx
@@ -1,3 +1,8 @@
+import AddProduct from "./AddProduct";
+import SearchProduct from "./SearchProduct";
+import ListProduct from "./ListProduct";
+import AddCategory from "./AddCategory";
+
 const Product = () => {
   return <div>Hello Product</div>;
 };
