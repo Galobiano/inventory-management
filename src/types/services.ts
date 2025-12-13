f2add66b8e39740c286a16ac63437d0eb3bce48a
Galobiano <151698267+Galobiano@users.noreply.github.com>
2025-12-13 21:44:50 +0800

    feat: add service

---
+++ src/types/services.ts
@@ -0,0 +1,11 @@
+export interface IParams {
+  [key: string]: unknown;
+}
+export interface IGenericOptions {
+  url: string;
+  params?: IParams;
+}
+export interface IErrorResponse {
+  status: string;
+  message: string;
+}
