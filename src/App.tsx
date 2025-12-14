3724cf6c246ae676a429f5c1b628b5783fb5ee6f
Galobiano <151698267+Galobiano@users.noreply.github.com>
2025-12-14 14:38:44 +0800

    feat: import AuthProvider + wrapper component

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,23 @@ import Dashboard from "./pages/dashboard/Dashboard";
 import UserManagement from "./pages/userManagement/UserManagement";
 import Product from "./pages/product/Product";
 import SaleReport from "./pages/report/SaleReport";
+import AuthProvider from "./context/AuthContext";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route element={<Weblayout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/user-management" element={<UserManagement />} />
-          <Route path="/product" element={<Product />} />
-          <Route path="/sale-report" element={<SaleReport />} />
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route element={<Weblayout />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/user-management" element={<UserManagement />} />
+            <Route path="/product" element={<Product />} />
+            <Route path="/sale-report" element={<SaleReport />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
