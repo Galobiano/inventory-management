def9f2bb45fc2b8014aef1fd0d28591ff95a7d38
Galobiano <151698267+Galobiano@users.noreply.github.com>
2025-12-28 13:42:00 +0800

    feat: added logout auth

--- src/pages/Logout.tsx
+++ src/pages/Logout.tsx
@@ -1,9 +1,12 @@
 import { FaPowerOff } from "react-icons/fa";
+import { useAuth } from "@/context/AuthContext";
 
 const Logout = () => {
+  const { logout } = useAuth();
+
   return (
     <div className="mt-auto flex pl-5 mb-5">
-      <button className="flex items-center gap-2">
+      <button className="flex items-center gap-2" onClick={logout}>
         <FaPowerOff className="mr-2" /> Logout
       </button>
     </div>
