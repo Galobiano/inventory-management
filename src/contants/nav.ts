fb2754dd4ea0a830ac831ffa7df3afb78487b69f
Galobiano <151698267+Galobiano@users.noreply.github.com>
2025-12-28 13:46:22 +0800

    feat: added label profile

--- src/contants/nav.ts
+++ src/contants/nav.ts
@@ -2,6 +2,7 @@ import { MdDashboard, MdOutlineProductionQuantityLimits } from "react-icons/md";
 import { FaUser } from "react-icons/fa";
 import { BsBarChartFill } from "react-icons/bs";
 import { IconType } from "react-icons";
+import { RiProfileFill } from "react-icons/ri";
 
 interface NavItem {
   label: string;
@@ -10,6 +11,7 @@ interface NavItem {
 }
 
 export const navItem: NavItem[] = [
+  { label: "Profile", href: "/profile", icon: RiProfileFill },
   { label: "Dashboard", href: "/dashboard", icon: MdDashboard },
   { label: "User Management", href: "/user-management", icon: FaUser },
   {
