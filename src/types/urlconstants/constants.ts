11f0b0aa8173f23f565c2d7eb6c463f9573c7c0b
Galobiano <151698267+Galobiano@users.noreply.github.com>
2025-12-29 15:38:25 +0800

    feat: added endpoint of sales

--- src/types/urlconstants/constants.ts
+++ src/types/urlconstants/constants.ts
@@ -3,6 +3,11 @@ export const LOGIN = "auth/login.php";
 export const GET_PRODUCTS = "product/get-product.php";
 export const GET_USERS = "user/get-users.php";
 export const GET_CATEGORIES = "categories/get-categories.php";
+export const GET_RECENT_SALES = "sales/recent-sales.php";
+export const GET_LATEST_SALES = "sales/latest-sales.php";
+export const GET_HIGHEST_SELLING_PRODUCTS =
+  "sales/highest-selling-products.php";
+export const GET_TOP_SELLING_PRODUCTS = "sales/top-selling-products.php";
 
 // ADD
 export const ADD_USER = "user/user.php";
