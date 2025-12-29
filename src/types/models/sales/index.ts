1f49a4f5a918c034ac31ecabddbc00391b090aae
Galobiano <151698267+Galobiano@users.noreply.github.com>
2025-12-29 15:33:36 +0800

    feat: add TypeScript interfaces for sales dashboard responses

---
+++ src/types/models/sales/index.ts
@@ -0,0 +1,47 @@
+// RECENT SALES
+export interface IRECENTSALES {
+  title: string;
+  total_price: string;
+  createddAt: string;
+}
+export interface IRECENTSALERESPONSE {
+  success: boolean;
+  data: IRECENTSALES[];
+}
+
+// LASTEST SALES
+export interface ILATESTSALES {
+  product_name: string;
+  quantity_sold: string;
+  total_price: string;
+  created_at: string;
+}
+
+export interface ILATESTSALERESPONSE {
+  success: boolean;
+  data: ILATESTSALES[];
+}
+
+// HIGHEST SELLING PRODUCTS
+export interface IHIGHESTSELLINGPRODUCTS {
+  title: string;
+  total_quantity: string;
+  total_revenue: string;
+}
+
+export interface IHIGHESTSELLINGPRODUCTSRESPONSE {
+  success: boolean;
+  data: IHIGHESTSELLINGPRODUCTS[];
+}
+
+// TOP SELLING PRODUCTS
+export interface ITOPSELLINGPRODUCTS {
+  title: string;
+  reamining_stock: string;
+  total_sold: string;
+}
+
+export interface ITOPSELLINGPRODUCTSRESPONSE {
+  success: boolean;
+  data: ITOPSELLINGPRODUCTS[];
+}
